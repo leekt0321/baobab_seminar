b06f40a9bcadf1fc3062470180d38402eab49e8b
leekt0321 <leekt0321@naver.com>
2025-06-17 01:29:23 -0400

    main.tf

--- terraform_aws/main.tf
+++ terraform_aws/main.tf
@@ -1,3 +1,15 @@
+terraform {
+  // terraform 버전이 1.0.0버전 이상 2.0.0버전 미만
+  required_version = ">= 1.0.0, < 2.0.0"
+  
+  required_providers {
+  aws = {
+    source = "hashicorp/aws"
+    // aws provider 버전이 4.0버전 이상인 경우 실행
+    version = "~> 4.0"
+    }
+  }
+}
 provider "aws" {
 region = "ap-northeast-2"
 }
