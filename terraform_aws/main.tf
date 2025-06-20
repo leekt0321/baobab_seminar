e9537aad4ac6ee13772ffe5f9d8bfb820554783e
leekt0321 <leekt0321@naver.com>
2025-06-20 02:54:21 -0400

    VPC

--- terraform_aws/main.tf
+++ terraform_aws/main.tf
@@ -14,6 +14,79 @@ provider "aws" {
 region = "ap-northeast-2"
 }
 
+# VPC
+resource "aws_vpc" "Seminar_VPC" {
+  cidr_block = "10.10.10.0/24"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "Seminar_VPC"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "Seminar_2a_public"{
+  vpc_id = aws_vpc.Seminar_VPC
+  cidr_block = "10.10.10.0/26"
+  tags = {
+    Name = "Seminar_Subnet_public"
+  }
+}
+resource "aws_subnet" "Seminar_2a_private"{
+  vpc_id = aws_vpc.Seminar_VPC
+  cidr_block = "10.10.10.64/26"
+  tags = {
+    Name = "Seminar_Subnet_pricate_1"
+  }
+}
+resource "aws_subnet" "Seminar_2b"{
+  vpc_id = aws_vpc.Seminar_VPC
+  cidr_block = "10.10.10.128/26"
+  tags = {
+    Name = "Seminar_Subnet_private_2"
+  }
+}
+resource "aws_subnet" "Seminar_2c"{
+  vpc_id = aws_vpc.Seminar_VPC
+  cidr_block = "10.10.10.192/26"
+  tags = {
+    Name = "Seminar_Subnet_private_3"
+  }
+}
+
+# IGW 
+resource "aws_internet_gateway" "Seminar_IGW" {
+  vpc_id = aws_vpc.Seminar_VPC
+  tags = {
+    Name = "Seminar_IGW"
+  }
+}
+
+# Elastic IP
+
+
+# NAT Gateway
+resource "aws_nat_gateway" "Seminar_NAT" {
+  
+  subnet_id = aws_subnet.Seminar_2a
+  tags = {
+    Name = "Seminar_NAT"
+  }
+
+  depends_on = [ aws_internet_gateway.Seminar_IGW ]
+}
+
+# Routing Table(public subnet)
+# Routing Table(private subnet)
+
+
+# Network ACL
+
+# SG(Security Group)
+
+
+# EC2
+/*
 resource "aws_instance" "name" {
   ami = "ami-0c593c3690c32e925"
   instance_type = "t2.micro"
@@ -21,3 +94,4 @@ resource "aws_instance" "name" {
     Name = "terraform-test"
   }
 }
+*/
