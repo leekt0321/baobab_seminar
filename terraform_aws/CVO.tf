86d2989c4329f5b9167dd36b77cf1d13fd097bf7
leekt0321 <leekt0321@naver.com>
2025-06-28 03:10:19 -0400

    VPC

---
+++ terraform_aws/CVO.tf
@@ -0,0 +1,12 @@
+/*
+
+* CVO 구성하는 방법
+1) NetApp 공식 Provider
+  -> NetApp Cloud Manager 계정 필요
+  -> NetApp Terraform Provider 설치 필요
+
+2) 수동 EC2 배포 후 CVO 설치(X)
+   -> 복잡
+   -> 그냥 ec2 생성 후 AMI로 구성하는 것.
+
+*/
