8c4d9c7256e1582851834ffa0547dbe37afbdbda
leekt0321 <leekt0321@naver.com>
2025-06-28 02:53:41 -0400

    VPC

--- terraform_aws/main.tf
+++ terraform_aws/main.tf
@@ -213,20 +213,6 @@ resource "aws_instance" "bastion_ec2" {
     chmod 400 seminar-key.pem
     echo "seminar-key.pem 생성 완료"
     EOT
-
+    interpreter = [ "/bin/bash", "-c" ]
   }
 }
-
-
-# output
-output "instance_public_ip"{
-  description = "EC2 인스턴스 퍼블릭 IP 주소"
-  value = aws_instance.bastion_ec2.public_ip
-}
-output "private_key" { // key 값 노출 방지
-  value = tls_private_key.ssh_key.private_key_pem
-  sensitive = true
-}
-
-
-# CVO
---
+++ terraform_aws/outputs.tf
@@ -0,0 +1,9 @@
+# output
+output "instance_public_ip"{
+  description = "EC2 인스턴스 퍼블릭 IP 주소"
+  value = aws_instance.bastion_ec2.public_ip
+}
+output "private_key" { // key 값 노출 방지
+  value = tls_private_key.ssh_key.private_key_pem
+  sensitive = true
+}
