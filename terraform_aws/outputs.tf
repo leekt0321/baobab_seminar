00de1d79335a5392ce6301d255af827be6eb6a51
leekt0321 <leekt0321@naver.com>
2025-07-01 05:38:38 -0400

    VPC

--- terraform_aws/outputs.tf
+++ terraform_aws/outputs.tf
@@ -7,6 +7,3 @@ output "private_key" { // key 값 노출 방지
   value = tls_private_key.ssh_key.private_key_pem
   sensitive = true
 }
-output "CVO_connector_ec2_private_ip" {
-  description = "CVO connector ec2 인스턴스 프라이빗 ip 주소"
-}
